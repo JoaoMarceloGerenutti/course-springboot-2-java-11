d131cf0a7eb6ee16306aff8dbaaa4712e6d10837
João Marcelo Gerenutti <joaomarcelogerenuttii@hotmail.com>
2022-09-21 18:51:45 -0300

    Order, Instant and ISO 8601.

---
+++ src/main/java/com/joaomarcelogerenutti/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.joaomarcelogerenutti.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joaomarcelogerenutti.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+	
+}
