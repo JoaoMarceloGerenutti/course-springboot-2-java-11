44b739a3a0158b497a6e15e2297478f4c295f7ff
João Marcelo Gerenutti <joaomarcelogerenuttii@hotmail.com>
2022-09-21 17:28:55 -0300

    H2 database, test profile and JPA.

--- src/main/java/com/joaomarcelogerenutti/course/entities/User.java
+++ src/main/java/com/joaomarcelogerenutti/course/entities/User.java
@@ -2,9 +2,17 @@ package com.joaomarcelogerenutti.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "test")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
